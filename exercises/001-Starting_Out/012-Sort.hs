019cd7b68bf58ee8f50c0eab4fab3d97f4586d6c
jorgeuliana1 <ulianamjjorge@gmail.com>
2020-02-01 23:16:12 -0200

    Quicksort algorithm implemented in Haskell

---
+++ exercises/001-Starting_Out/012-Sort.hs
@@ -0,0 +1,17 @@
+-- My quick sort algorithm in Haskell (most common approach)
+quickSort :: (Ord a) => [a] -> [a]
+quickSort [] = [] -- In case the list is empty
+
+-- Now, this x:xs is amazing!
+-- If we input [a, b, c, d]
+-- The function will understand x = a, xs = [b, c, d]
+quickSort(x:xs) = do
+    -- Quicksort's partition right here
+    let smaller = quickSort [ n | n <- xs, n <= x ]
+    let bigger  = quickSort [ n | n <- xs, n  > x ]
+
+    -- Merging both partitions
+    smaller ++ [x] ++ bigger
+
+main = print(quickSort [1231, 12313, 41123, 124, 12, 123, 31, 2313])
+-- outputs: [12,31,123,124,1231,2313,12313,41123]
