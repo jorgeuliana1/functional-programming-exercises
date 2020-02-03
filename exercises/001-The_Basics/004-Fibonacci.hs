d0db3a9f94b288b7c58bcdd5e8916ef387a7f5c5
jorgeuliana1 <ulianamjjorge@gmail.com>
2020-02-03 19:05:32 -0200

    Recursive Fibonacci function

--- exercises/001-The_Basics/004-Fibonacci.hs
+++ exercises/001-The_Basics/004-Fibonacci.hs
@@ -1,13 +1,12 @@
-getFibo num = -- This function returns a list containing the fibonacci sequence numbers in order
+getFibo num -- This function returns a list containing the fibonacci sequence numbers in order
     -- This function is recursive
     -- Conditional structures are like this:
-    if num == 0 then [1]
-        else if num == 1 then [1, 1]
-            else do
-                let pastFibo = getFibo(num - 1)
-                let fiboLen = length pastFibo
-                pastFibo ++ [(pastFibo !! (fiboLen - 1) + pastFibo !! (fiboLen - 2))]
+    | num == 0 = [1]
+    | num == 1 = [1, 1]
+    | otherwise = fibo
+    where
+        pastFibo = getFibo(num - 1)
+        fiboLen = length pastFibo
+        fibo = pastFibo ++ [(pastFibo !! (fiboLen - 1) + pastFibo !! (fiboLen - 2))]
 
-main =
-    do let x = getFibo 10
-       print x
+main = print(getFibo 10)
