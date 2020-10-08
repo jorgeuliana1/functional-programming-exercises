84b134f9a962f7c2fe87383f9a08325ba91821b8
jorgeuliana1 <ulianamjjorge@gmail.com>
2020-10-07 21:25:09 -0300

    Missing the last one of the fifth list

--- exercises/999-College_Lists/aula05.hs
+++ exercises/999-College_Lists/aula05.hs
@@ -39,3 +39,48 @@ multiTableLL n = [[(i, j, i * j) | i <- [0..9]] | j <- [0..n]]
 -- i. Obtain the multiplication table of two lists:
 multiTableCL :: [Int] -> [Int] -> [(Int, Int, Int)]
 multiTableCL xs ys = [(i, j, i * j) | i <- xs, j <- ys]
+
+-- j. Obtain the indexes of a triangular matrix, line by line.
+indTriSup :: Int -> Int -> [(Int, Int)]
+indTriSup n m = [(i, j) | i <- [0..n-1], j <- [i..m-1]]
+
+-- k. Obtain the indexes of a triangular matrix, line by line.
+indTriInf :: Int -> Int -> [(Int, Int)]
+indTriInf n m = [(i, j) | i <- [0..n-1], j <- [0..i]]
+
+-- l. Obtain the combinations of a pythagorean triangle with sides smaller than n.
+pythagoras :: Int -> [(Int, Int, Int)]
+pythagoras n = [(a, b, c) | a <- [1..n], b <- [1..n], c <- [1..n], a ^ 2 + b ^ 2 == c ^ 2]
+
+-- m. Present the factors of an Integer positive number.
+factors :: Int -> [Int]
+factors n = [i | i <- [1..n], n `mod` i == 0]
+
+-- n. Obtain prime numbers smaller than n.
+primes :: Int -> [Int]
+primes n = [i | i <- [2..n], length ([j | j <- [2..i - 1], i `mod` j == 0]) == 0]
+
+-- o. Obtain perfect numbers smaller than n.
+perfects :: Int -> [Int]
+perfects n = [i | i <- [1..n], sum(tail(reverse(factors i))) == i]
+
+-- p. Count the occurrences of an element in a list.
+countElem :: (Eq a) => a -> [a] -> Int
+countElem x xs = length [e | e <- xs, e == x]
+
+-- q. Verify if a list is ordered.
+order :: (Ord a) => [a] -> Bool
+order xs =
+    length [ True | i <- [0..length xs - 1], (length [j | j <- take i xs, xs !! i > j]) == (length (take i xs))] == length xs
+
+-- r. Calculate the scalar product of two vectors in the same dimension.
+scalar :: [Int] -> [Int] -> Int
+scalar xs ys = sum [xs !! i * ys !! i | i <- [0..length xs - 1]]
+
+-- s. Obtain the positions where elements are found in a list.
+positions :: (Eq a) => a -> [a] -> [Int]
+positions x xs = [i | i <- [0..length xs - 1], xs !! i == x]
+
+-- t. Generate a list containing the first prime numbers.
+isPrime :: Int -> Bool
+isPrime n = length [True | i <- [2..n - 1], n `mod` i == 0] == 0
