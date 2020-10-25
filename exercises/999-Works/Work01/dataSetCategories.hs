ed9ac6431cd592b0bb23f4e21ab8685a44116977
jorgeuliana1 <ulianamjjorge@gmail.com>
2020-10-25 18:43:31 -0300

    dataSetCategories.hs commented

--- exercises/999-Works/Work01/dataSetCategories.hs
+++ exercises/999-Works/Work01/dataSetCategories.hs
@@ -1,11 +1,28 @@
 module DataSetCategories where
 import DataSet
 
--- Returns the amount of samples of a certain category (adapted for Iris DataSet)
+{-
+Returns the amount of samples of a certain category.
+# Input
+dataSet :: IrisDataSet (The data set to be analyzed)
+category :: IrisCategory (The category to be analyzed)
+# Output
+length :: Int (The number of elements of a certain category in the data set)
+-}
 categoryLength :: IrisDataSet -> IrisCategory -> Int
 categoryLength dataSet category = sum [ 1 | (_, _, _, _, samCat) <- dataSet, samCat == category ]
 
--- Returns a list of the categories contained in the given dataset
+{-
+Returns a list of the categories contained in the given data set (no repetitions).
+# Input
+dataSet :: IrisDataSet (The data set to be analyzed)
+currentIndex :: Int (The current index of the "iteration"
+                    (Start with the length of the data set))
+categories :: [IrisCategory] (List of categories found
+                              until the current "iteration")
+# Output
+categories :: [IrisCategory] (List of the categories contained in the data set)
+-}
 dataSetCategoriesList :: IrisDataSet -> Int -> [IrisCategory] -> [IrisCategory]
 dataSetCategoriesList dataSet 1 categories = do
     let (_, _, _, _, currentCategory) = dataSet !! 0
@@ -20,6 +37,12 @@ dataSetCategoriesList dataSet currentIndex categories = do
         else
             dataSetCategoriesList dataSet (currentIndex - 1) (currentCategory:categories)
 
--- An elegant wrapper for dataSetCategoriesList.
+{-
+An elegant wrapper for dataSetCategoriesList.
+# Input
+dataSet :: IrisDataSet (The data set to be analyzed)
+# Ouput
+categories :: [IrisCategory] (The categories of the data set)
+-}
 dataSetCategories :: IrisDataSet -> [IrisCategory]
 dataSetCategories dataSet = dataSetCategoriesList dataSet (length dataSet) []
