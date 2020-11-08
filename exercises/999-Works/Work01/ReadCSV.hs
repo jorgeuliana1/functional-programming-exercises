c73d30cfeefd5f8d5eb8e156b70355e938ca6f5a
jorgeuliana1 <ulianamjjorge@gmail.com>
2020-11-07 21:37:10 -0300

    Created the splitString function and commented it :sunglasses:

--- exercises/999-Works/Work01/ReadCSV.hs
+++ exercises/999-Works/Work01/ReadCSV.hs
@@ -1,10 +1,25 @@
 module ReadCSV where
-import Data.List.Split
+
+{--
+Takes a string and splits it in sections divided by a separator.
+# Input:
+str :: String (String to be splitted.)
+sep :: Char   (The separator to be used as reference.)
+# Output:
+strs :: [String] (Splitted strings)
+--}
+splitString :: String -> Char -> [String]
+splitString [] sep = [""]
+splitString (currChar:restStr) sep
+    | currChar == sep = "":rest
+    | otherwise = (currChar:(head rest)):(tail rest)
+    where
+        rest = splitString restStr sep
 
 {-
 Reads a CSV file and returns the data.
 # Input:
-fp :: FilePath (Path of the CSV file)
+fp :: FilePath (Path of the CSV file.)
 # Output:
 csvContent :: IO [[String]] (List where each element corresponds to
                              a line on the CSV file, each "line" is
@@ -15,4 +30,4 @@ readCSV :: FilePath -> IO [[String]]
 readCSV fp = do
     rawCSV <- readFile fp
     let csvLines = lines rawCSV
-    return [(splitOn "," csvLine) | csvLine <- csvLines]
+    return [(splitString csvLine ',') | csvLine <- csvLines]
